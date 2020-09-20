09b2aaf45ef2d897199c9225b227420cdb66b6e8
racoon-dev <59138827+escojwd@users.noreply.github.com>
2020-09-20 15:17:27 +0400

    Update setTime.js

--- setTime.js
+++ setTime.js
@@ -1,24 +1,12 @@
-// Task
-// 1. set a time with "now time";
-// 2. choose "till time";
-// 3. make "countdown time";
-
-
-
 let presentDate = new Date();
 
 let newDate = new Date("Sep 20 2020 23:59:59");
 
 let timeLeft = Math.floor((newDate - presentDate)/1000);
 
-// let myinterval = setInterval(betweenTime, 5000);
-
 let seconds = timeLeft;
 
 function betweenTime(seconds){
-    
-    seconds -= 1000;
-    
     let minutes = Math.floor(seconds/60);
     let hours = Math.floor(minutes/60);
     let day = Math.floor(hours/24);
@@ -28,7 +16,6 @@ function betweenTime(seconds){
     let result = "";
     
     let countedSeconds = seconds % 60;
-    
     
     minutes = minutes % 60;
     hours = hours % 24;
@@ -66,21 +53,8 @@ function betweenTime(seconds){
     return result;
 }
 
-// setInterval(betweenTime)*1000;
-
-// console.log(presentDate);
-
-// console.log(newDate);
-
 setInterval(function(){ 
     // betweenTime(seconds -= 1)
     console.clear(betweenTime(seconds));
     console.log(betweenTime(seconds -= 1));
 }, 1000);
-
-// console.log(seconds);
-
-
-
-
-// console.log(y)
